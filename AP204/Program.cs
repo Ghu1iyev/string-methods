f6753a24b536ac34ad70e3ff86f25bff916dfcd4
Ghu1iyev <tu12n4c1r@code.edu.az>
2022-03-15 12:14:31 +0400

    string-methods

---
+++ AP204/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AP204
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+
+
+            string str = "bU tasKI metHodla yAzmAliSiz";
+
+            string lower = str.ToLower();
+
+            string sentences = lower.Replace("b", "B");
+            sentences = sentences.Replace("task", "Task");
+            sentences = sentences.Replace("methodla", "Methodla");
+            sentences = sentences.Replace("y", "Y");
+
+            
+            Console.WriteLine(sentences);
+
+
+
+        }
+    }
+}
